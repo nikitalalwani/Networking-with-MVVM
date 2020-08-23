973757388c638bee105006975e4f1a83bb959258
Nikita <nikitalalwani93@gmail.com>
2020-08-22 20:44:31 -0400

    Initial Commit

---
+++ TopMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopMovies
+//
+//  Created by nikita lalwani on 8/22/20.
+//  Copyright © 2020 nikita lalwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
